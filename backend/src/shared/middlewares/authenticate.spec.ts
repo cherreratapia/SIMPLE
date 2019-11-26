733226710dca167ae0fba97c8fae30a99a52c2f0
Cristopher Herrera <c.herreratapia@hotmail.com>
2019-11-26 00:45:11 -0300

    TDD fireauth

--- backend/src/shared/middlewares/authenticate.spec.ts
+++ backend/src/shared/middlewares/authenticate.spec.ts
@@ -13,7 +13,7 @@ describe("Middlewares", () => {
       };
       admin.auth().verifyIdToken = jest.fn().mockReturnValue({});
 
-      await authenticate(admin);
+      await authenticate(admin)(req, res, next);
       expect(req.header.mock.calls).toEqual([["authToken"]]);
       expect(next.mock.calls).toEqual([[]]);
     });
@@ -32,7 +32,7 @@ describe("Middlewares", () => {
       admin.auth().verifyIdToken = jest.fn().mockImplementation(() => {
         throw new Error("Error mock");
       });
-      await authenticate(admin);
+      await authenticate(admin)(req, res, next);
       expect(req.header.mock.calls).toEqual([["authToken"]]);
       expect(res.sendStatus.mock.calls).toEqual([[403]]);
       expect(next.mock.calls).toEqual([]);
--- backend/src/shared/middlewares/authenticate.ts
+++ backend/src/shared/middlewares/authenticate.ts
@@ -3,11 +3,7 @@ const authenticate = (admin: any) => {
   return async (req: any, res: any, next: NextFunction) => {
     try {
       const token: any = req.header("authToken");
-      const result = await admin
-        .auth()
-        .verifyIdToken(
-          "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTU3NDczOTU1NCwiZXhwIjoxNTc0NzQzMTU0LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1hcXA1aUBzaW1wbGUtcmlwbGV5LmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstYXFwNWlAc2ltcGxlLXJpcGxleS5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6InRHbXVRalFJcGtlNlo5eTljWHVXMmZBMjRuSTIifQ.q2DKNrRqPIJ_LxNLydTXXvERoGUc6BAujVANZ6setDbz5wnzxKlJa1INcpAa0NH0cIAWTJHMJD_z9_Wc7T5HgllGoNhxzmL4u_ZgakvLcgHltgGdOD4w7vlrtfY8h-hM_Bet9xVMicFmrpVDuHN4C0QZkN8NQ4bxGAchCLJu3UesgklZ-jKYQvaxG4mwPX8kEEFLADhNB1YmzMArw8dUBhmFTWWlMU5DVac-ILiLwxmdR_yJ8OmL3xILC1vhpcnDKsJtREcmyqCFEpvs4L5ymcgy6DXwHaybARAqx207e-zKsfbAmEBzsL1AQzAzNMdE7SAYShTpJvknFB8A3Fvsxg"
-        );
+      const result = await admin.auth().verifyIdToken(token);
       console.log("result", result);
       if (result) {
         next();
