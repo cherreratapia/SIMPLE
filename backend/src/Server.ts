8ffd1ef20d5bf57fae3927bd9407192c830a1679
Cristopher Herrera <c.herreratapia@hotmail.com>
2019-11-26 00:42:29 -0300

    Creando test para middleware de auth

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -6,9 +6,22 @@ import path from "path";
 import BaseRouter from "./routes";
 import redis, { RedisError } from "redis";
 import fetch from "node-fetch";
+import admin from "firebase-admin";
+import authenticate from "./shared/middlewares/authenticate";
+const serviceAccount = require("../config/simple-ripley-firebase-adminsdk-aqp5i-2e5cbd2316.json");
 
 // Init express
 const app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://simple-ripley.firebaseio.com"
+});
+
+//token
+/* 
+  eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTU3NDczOTU1NCwiZXhwIjoxNTc0NzQzMTU0LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1hcXA1aUBzaW1wbGUtcmlwbGV5LmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstYXFwNWlAc2ltcGxlLXJpcGxleS5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6InRHbXVRalFJcGtlNlo5eTljWHVXMmZBMjRuSTIifQ.q2DKNrRqPIJ_LxNLydTXXvERoGUc6BAujVANZ6setDbz5wnzxKlJa1INcpAa0NH0cIAWTJHMJD_z9_Wc7T5HgllGoNhxzmL4u_ZgakvLcgHltgGdOD4w7vlrtfY8h-hM_Bet9xVMicFmrpVDuHN4C0QZkN8NQ4bxGAchCLJu3UesgklZ-jKYQvaxG4mwPX8kEEFLADhNB1YmzMArw8dUBhmFTWWlMU5DVac-ILiLwxmdR_yJ8OmL3xILC1vhpcnDKsJtREcmyqCFEpvs4L5ymcgy6DXwHaybARAqx207e-zKsfbAmEBzsL1AQzAzNMdE7SAYShTpJvknFB8A3Fvsxg
+*/
 
 // Create client of redis
 const client = redis.createClient(6379);
--- backend/src/shared/middlewares/authenticate.spec.ts
+++ backend/src/shared/middlewares/authenticate.spec.ts
@@ -1,1 +1,41 @@
-describe("Authenticate ", () => {});
+import authenticate from "./authenticate";
+
+describe("Middlewares", () => {
+  describe("Authenticate", () => {
+    it("logged in", async () => {
+      const req = {
+        header: jest.fn().mockReturnValue("1")
+      };
+      const res = {};
+      const next = jest.fn();
+      const admin = {
+        auth: jest.fn().mockReturnThis()
+      };
+      admin.auth().verifyIdToken = jest.fn().mockReturnValue({});
+
+      await authenticate(admin);
+      expect(req.header.mock.calls).toEqual([["authToken"]]);
+      expect(next.mock.calls).toEqual([[]]);
+    });
+
+    it("Should be unauthorized", async () => {
+      const req = {
+        header: jest.fn().mockReturnValue("wrongUser")
+      };
+      const res = {
+        sendStatus: jest.fn()
+      };
+      const next = jest.fn();
+      const admin = {
+        auth: jest.fn().mockReturnThis()
+      };
+      admin.auth().verifyIdToken = jest.fn().mockImplementation(() => {
+        throw new Error("Error mock");
+      });
+      await authenticate(admin);
+      expect(req.header.mock.calls).toEqual([["authToken"]]);
+      expect(res.sendStatus.mock.calls).toEqual([[403]]);
+      expect(next.mock.calls).toEqual([]);
+    });
+  });
+});
