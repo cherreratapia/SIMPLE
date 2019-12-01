7b9768ae66789c3bac0de15a72992f473cf35e59
Cristopher Herrera <c.herreratapia@hotmail.com>
2019-11-30 22:44:56 -0300

    Intento de docker nº1

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAlZ8rl2cJvYelAmAZp6G1QujIiNGDrDwU",
+    authDomain: "simple-ripley-9c988.firebaseapp.com",
+    databaseURL: "https://simple-ripley-9c988.firebaseio.com",
+    projectId: "simple-ripley-9c988",
+    storageBucket: "simple-ripley-9c988.appspot.com",
+    messagingSenderId: "60697058788",
+    appId: "1:60697058788:web:c35de15b7643e69e64bbf1"
+  },
+  url: "http://localhost:3000/api/",
+  v: "v1/"
 };
