9c11c232f87de0151f8759e9540b8da30ea7b30c
Cristopher Herrera <c.herreratapia@hotmail.com>
2019-12-01 19:42:46 -0300

    revision del auth

--- backend/src/shared/middlewares/authenticate.ts
+++ backend/src/shared/middlewares/authenticate.ts
@@ -8,11 +8,14 @@ const authenticate = (admin: admin.app.App) => {
       const token: any = req.header("Authorization");
       const responseFireAuth = await admin.auth().verifyIdToken(token);
       if (responseFireAuth) {
+        console.log("if de auth");
         next();
       } else {
+        console.log("else en auth");
         return res.sendStatus(403);
       }
     } catch (error) {
+      console.log("error en auth", error);
       logger.error(`Error al verificar token ${error.message}`);
       return res.sendStatus(403);
     }
